bb366bd2014f27981a3280611b2b06ed194ae0a0
OlgaPetrova3103 <73943461+OlgaPetrova3103@users.noreply.github.com>
2021-04-18 21:45:12 +0300

    Update Program.cs

--- Life/Program.cs
+++ Life/Program.cs
@@ -50,6 +50,47 @@ namespace cli_life
             ConnectNeighbors();
             Randomize(liveDensity);
         }
+        
+        public Board(int value, int width, int height, int cellSize, double liveDensity = .1)
+        {
+            CellSize = cellSize;
+            if (value == 0)
+            {
+                Cells = new Cell[width / cellSize, height / cellSize];
+                for (int x = 0; x < Columns; x++)
+                    for (int y = 0; y < Rows; y++)
+                        Cells[x, y] = new Cell();
+
+                ConnectNeighbors();
+                Randomize(liveDensity);
+            }
+        
+        else//read file
+            {
+                using (StreamReader r = new StreamReader("test.txt"))
+                {
+                    List<string> matr = new List<string>();
+                    string curstr;
+                    int max = 0;
+                    while ((curstr = r.ReadLine()) != null)
+                    {
+                        matr.Add(curstr);
+                        if (curstr.Length > max) max = curstr.Length;
+                    }
+                    if (max == 0) return;
+                    Cells = new Cell[max / cellSize, matr.Count/ cellSize];
+                    for (int x = 0; x < Columns; x++)
+                        for (int y = 0; y < Rows; y++)
+                        {
+                            if (matr[y].Length <= x)
+                                Cells[x, y] = new Cell(0);
+                            else if (matr[y][x] == '*') Cells[x, y] = new Cell(1);
+                            else Cells[x, y] = new Cell(0);
+                        }
+                    ConnectNeighbors();
+                }
+            }
+        }
 
         readonly Random rand = new Random();
         public void Randomize(double liveDensity)
@@ -89,171 +130,103 @@ namespace cli_life
             }
         }
     }
-    public class BThread
-    {
-        Thread BThread, stop;
-        public bool run = false;
-        string options;
-
-        public bThread(string optionsFile)
-        {
-            options = optionsFile;
-            BThread = new Thread(this.boardRun);
-            stop = new Thread(this.pause);
-        }
-    }
-    
-            public string ToString()
-        {
-            string res = "";
-            for (int row = 0; row < Rows; row++)
-            {
-                for (int col = 0; col < Columns; col++)
-                {
-                    var cell = Cells[col, row];
-                    if (cell.IsAlive)
-                    {
-                        res+="*";
-                    }
-                    else
-                    {
-                        res+=" ";
-                    }
-                }
-                res+="\n";
-            }
-            return res;
-        }
-
-        public void setState(string state)
-        {
-            string[] subRows = state.Split('\n');
-            for (int row = 0; row < Rows; row++)
-            {
-                for (int col = 0; col < Columns; col++)
-                {
-                    char s = subRows[row][col];
-                    var cell = new Cell();
-                    if (s == '*')
-                    {
-                        cell.IsAlive = true;
-                    }
-                    else
-                    {
-                        cell.IsAlive = false;
-                    }
-                    Cells[col, row] = cell;
-                }
-            }
-            ConnectNeighbors();
-        }
-    }
     
     
     class Program
     {
+              
+{
+        public class Item
+        {
+            public int width;
+            public int height;
+            public int cellSize;
+            public double liveDensity;
+        }
         static Board board;
-        static Thread BoardThread, stop;
-        static public bool run = true;
-        static object lockOn;
-
-        static public void startThreads()
+        static private void Reset(int val)
         {
-            BoardThread = new Thread(boardRun);
-            stop = new Thread(pause);
-            lockOn = new object();
-            BThread.Start();
-            stop.Start();
-        }
-
-        static void boardRun()
-        {
-            while (true)
+            using (StreamReader r = new StreamReader("life.json"))
             {
-                if (run)
+                Item items = new Item();
+                string str = r.ReadToEnd();
+                string tmp_str = "";
+                int i = 0;
+                while (str[i] != ':') i++;
+                i++;
+                while (str[i] != ',')
                 {
-                    lock (lockOn)
-                    {
-                        Console.SetCursorPosition(0, 0);
-                        Render();
-                        board.Advance();
-                        Thread.Sleep(100);
-                    }
+                    tmp_str = tmp_str + str[i];
+                    i++;
                 }
-            }
-        }
-
-        static void pause()
-        {
-            while (true)
-            {
-                Console.ReadKey();
-                lock (lockOn)
+                items.width = Int32.Parse(tmp_str);
+                while (str[i] != ':') i++;
+                i++;
+                tmp_str = "";
+                while (str[i] != ',')
                 {
-                    run = false;
-                    while (true)
-                    {
-                        Console.Clear();
-                        Render();
-                        Console.WriteLine("\npress 1 to save, 2 to load or any other key to continue\n");
-                        char c = Console.ReadKey().KeyChar;
-                        if (c == '1')
-                        {
-                            Console.WriteLine("\nenter file name (with .txt)\n");
-                            string fileName = Console.ReadLine();
-                            saveToFile(fileName);
-                        }
-                        else if (c == '2')
-                        {
-                            Console.WriteLine("\nenter file name (with .txt)\n");
-                            string fileName = Console.ReadLine();
-                            loadFromFile(fileName);
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            run = true;
-                            break;
-                        }
-                    }
+                    tmp_str = tmp_str + str[i];
+                    i++;
                 }
-            }
-        }
-        static private void Reset(string optionsFile = "")
-        {
-            if (optionsFile == "")
-            {
+                items.height = Int32.Parse(tmp_str);
+                while (str[i] != ':') i++;
+                i++;
+                tmp_str = "";
+                while (str[i] != ',')
+                {
+                    tmp_str = tmp_str + str[i];
+                    i++;
+                }
+                items.cellSize = Int32.Parse(tmp_str);
+                while (str[i] != ':') i++;
+                i++;
+                tmp_str = "";
+                while (str[i] != '.')
+                {
+                    tmp_str = tmp_str + str[i];
+                    i++;
+                }
+                i++;
+                items.liveDensity = Int32.Parse(tmp_str);
+                tmp_str = "";
+                while (str[i] != '\r')
+                {
+                    tmp_str = tmp_str + str[i];
+                    i++;
+                }
+                int st = 1;
+                for (int j = 0; j < tmp_str.Length; j++) st = st * 10;
+                items.liveDensity = items.liveDensity + (float)Int32.Parse(tmp_str) / st;
                 board = new Board(
-                    width: 50,
-                    height: 20,
-                    cellSize: 1,
-                    liveDensity: 0.5);
-            } else
-            {
-                string optionsString = File.ReadAllText(optionsFile);
-                Dictionary<string, double>  options = JsonSerializer.Deserialize<Dictionary<string, double>>(optionsString);
-                board = new Board((int)options["width"], (int)options["height"], (int)options["cellSize"], options["liveDensity"]);
+                    val,
+                    width: items.width,
+                    height: items.height,
+                    cellSize: items.cellSize,
+                    liveDensity: items.liveDensity);
             }
         }
         static void Render()
         {
-            Console.Write(board.ToString());
+            using (StreamWriter r = new StreamWriter("test_1.txt"))
+            {
+                for (int row = 0; row < board.Rows; row++)
+                {
+                    for (int col = 0; col < board.Columns; col++)
+                    {
+                        var cell = board.Cells[col, row];
+                        if (cell.IsAlive)
+                        {
+                            Console.Write('*');
+                            r.Write('*');
+                        }
+                        else
+                        {
+                            Console.Write(' ');
+                            r.Write(' ');
+                        }
+                    }
+                    Console.Write('\n');
+                    r.Write('\n');
+                }
+            }
         }
-
-        static void saveToFile(string fileName)
-        {
-            File.WriteAllText(fileName, board.ToString());
-        }
-
-        static void loadFromFile(string fileName)
-        {
-            string state = File.ReadAllText(fileName);
-            board.setState(state);
-        }
-        static void Main(string[] args)
-        {
-            Reset("../../../life.json");
-            startThreads();
-        }
-    }
-}
